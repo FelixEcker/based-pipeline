f3b2749e64c1c44995c78dcec7d37e4ad3854c82
Nycz-lab <nyczdesignz@gmail.com>
2022-12-09 18:12:18 +0100

    wait until ms unixtime and play based.mp3

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,19 @@
+use core::time;
 use std::error::Error;
 use tokio::net::TcpStream;
 use std::io;
 use std::io::Write;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::str;
+
+// timing
+use std::time::{Duration, SystemTime};
+use std::thread;
+
+// audio playback
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
 
 
 #[tokio::main]
@@ -10,11 +21,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // let socket = TcpSocket::new_v4()?;
 
-    let mut stream = TcpStream::connect("127.0.0.1:5000").await?;
-    println!("Success connecting to 127.0.0.1:5000");
+    // prepare Sound stream
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open("based.mp3").unwrap());
+    let source = Decoder::new(file).unwrap();
+    
 
-    let mut b1 = [0; 10];
-    let mut b2 = [0; 10];
+    let mut stream = TcpStream::connect("127.0.0.1:5001").await?;
+    println!("Success connecting to 127.0.0.1:5001");
+
+    let mut b1 = [0; 13];
+    let mut b2 = [0; 13];
 
     // Peek at the data
     let n = stream.peek(&mut b1).await?;
@@ -23,6 +40,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     assert_eq!(n, stream.read(&mut b2[..n]).await?);
     assert_eq!(&b1[..n], &b2[..n]);
-    stream.write_all(b"Hello World!").await?;
+    println!("{:?}", b2);
+
+    let timestamp = str::from_utf8(&b2)?;           // get string representation of ascii codes
+    let timestamp: u64 = timestamp.parse().unwrap();      // parse string to long    
+    
+    let mut now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u64;    // get local unix timestamp as millis kekw this could panic in 2077 or so
+    let sleepTimer = time::Duration::from_millis(timestamp - now);
+    
+    println!("Sound deadline from remote⏰: {}", timestamp);
+    println!("Current time⌚️: {}", now);
+ 
+    thread::sleep(sleepTimer);  // yes this is bad oops
+
+    now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u64;
+    println!("now its⌚️: {}!", now);
+
+    stream_handle.play_raw(source.convert_samples())?;  // play basedness sound
+    thread::sleep(time::Duration::from_secs(5));        // sleep for 5 secs TODO get file track duration
+
+    //stream.write_all(b"Hello World!").await?;
     Ok(())
 }
